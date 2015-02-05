6fb01bce8ad2b47a75433c5ef2ea81361dfd3b52
Sławomir Chrobak <slawomir.chrobak@gmail.com>
2015-02-05 22:39:54 +0100

    Event handler interface

---
+++ src/Handler/EventHandler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Behat\Reporter\Handler;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Class EventHandler
+ * @package Behat\Reporter\Handler
+ */
+class EventHandler
+{
+    /**
+     * @param Event $event
+     */
+    public function handle(Event $event)
+    {
+    }
+}
